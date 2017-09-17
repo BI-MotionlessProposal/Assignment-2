f4dccbc3647839f528305f0518a150ac1e8bcfb0
unknown <athinodoros.sgouromallis@hotmail.com>
2017-09-17 23:06:34 +0200

    mb kb

--- 4_1_min.sh
+++ 4_1_min.sh
@@ -4,17 +4,18 @@ max2=""
 temp=0
 words=""
 list=$(find ./data -name '*.csv')	
-for file in $list; do
-	temp=`wc ${file}| awk '{print $1}'`
-	words2=`wc ${file}| awk '{print $4}'`
+for file in $list
+ do
+	temp=$(wc ${file}| awk '{print $1}')
+	words2=$(wc ${file}| awk '{print $4}')
 	if [[ "$temp" -lt "$max" ]]
 		then
 		max=$temp
 		max2=$words2
 	fi	
 done
-actualsize=$(stat -c%s "$max2")
+actualsize=$(($(stat -c%s "$max2")/1024/1024))
 sales=$(tail -n +1 "$max2" | wc -l)
-echo "zip was : " + "$max2" "the file-size was" "$actualsize"  " the sales were :  $sales"
+echo "zip was : " + "$max2" ". The file-size was" "$actualsize"  "mb. The sales were :  $sales"
 
 
