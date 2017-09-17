e20bbc458a5d616ba839c654df5b10f59abda946
unknown <athinodoros.sgouromallis@hotmail.com>
2017-09-17 18:58:46 +0200

    question 4 scripts

---
+++ 4.1 max.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+max=0
+max2=""
+temp=0
+words=""
+list=$(find . -name '*.csv')	
+for file in $list; do
+	temp=`wc ${file}| awk '{print $1}'`
+	words2=`wc ${file}| awk '{print $4}'`
+	if [[ "$temp" -gt "$max" ]]
+		then
+		max=$temp
+		max2=$words2
+	fi	
+done
+actualsize=$(stat -c%s "$max2")
+sales=$(tail -n +1 "$max2" | wc -l)
+echo "zip was : "  "$max2" "the file-size was" "$actualsize"  " the sales were : " "$sales" 
+
---
+++ 4.1 mn.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+max=10000000000000000000000
+max2=""
+temp=0
+words=""
+list=$(find . -name '*.csv')	
+for file in $list; do
+	temp=`wc ${file}| awk '{print $1}'`
+	words2=`wc ${file}| awk '{print $4}'`
+	if [[ "$temp" -lt "$max" ]]
+		then
+		max=$temp
+		max2=$words2
+		echo "new max"
+	fi	
+done
+actualsize=$(stat -c%s "$max2")
+sales=$(tail -n +1 "$max2" | wc -l)
+echo "zip was : " + "$max2" "the file-size was" "$actualsize"  " the sales were :  $sales"
+
+
